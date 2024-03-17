8e0702e6ad430862fe81c5b5365a111d64876c46
sidharths <sidharthsuresh599@gmail.com>
2024-03-17 20:36:31 +0530

    adminside

--- hrapi/admin.py
+++ hrapi/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from hrapi.models import Hr,Teams,ProjectDetail,TaskChart,Performance_assign
+from hrapi.models import Hr,Teams,ProjectDetail,TaskChart,Performance_assign,CustomUser
 
 # Register your models here.
 
 
 admin.site.register(Hr)
-admin.site.register(Teams)
+admin.site.register(CustomUser)
 admin.site.register(Performance_assign)
+
